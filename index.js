98213bd98145d02b1788553cc5f6edc497b49f60
leonardo Quintero <lleo5301@gmail.com>
2015-10-29 21:21:40 -0400

    api for images

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    fs = require('fs');
 
 var app = express();
 
@@ -12,6 +13,14 @@ app.use(bodyParser.json());
 app.use('/', express.static('./public'));
 
 app.use('/bower_components', express.static('./bower_components'));
+
+//simple api to return image links
+app.get('/api/backgrounds', function(req,res){
+	fs.readdir('./public/img', function(err, data){
+		res.json({data:data});
+	})
+	
+})
 
 //listen
 app.listen(8080);
