5506d462c6fc4425af02ffce43f657ae9d2a7dcc
leonardo Quintero <lleo5301@gmail.com>
2015-11-02 10:11:52 -0500

    finish up home controller

--- public/app/controllers/homeController.js
+++ public/app/controllers/homeController.js
@@ -1,29 +1,24 @@
 //home controller
 app.controller('homeController', ['$scope', '$http', '$interval', '$timeout', function($scope, $http, $interval, $timeout){
 	console.log('controller running');
-	$scope.heading =  "Hello my name is Leonardo Quintero and I'm a Full-Stack Developer";
+	$scope.heading =  "Hello my name is Leonardo and I'm a Full-Stack Developer";
+	$scope.img = "optimized/background6.jpg";
+	$scope.showText = false;
+
+	//get icons
+	$http.get('../api/icons').then(setIcons);
+
+	// animation for text
+	$timeout(function() {
+        $scope.showText = true;
+        $scope.ready=true;
+    }, 1000);
 
 
-	//lets get the backgrounds from the api
-	$http.get('../api/backgrounds').then(setBackgrounds);
 
-	function setBackgrounds(res){
-		var i_count = 0;
-		// console.log(res.data);
-		$scope.backgrounds = res.data.data;
-		console.log($scope.backgrounds);
-
-		$interval(function(){
-			if(i_count > $scope.backgrounds.length -1 ){
-				i_count = 0;
-			}
-			$scope.img = 'img/'+$scope.backgrounds[i_count];
-			console.log($scope.img);
-			i_count ++;
-
-		}, 10000);
-		
-
+	//set icons
+	function setIcons(res){
+		$scope.icons = res.data.data;
 	}
 
 
