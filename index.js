8603533a8966a5d82234287e9263011430a40a64
leonardo Quintero <lleo5301@gmail.com>
2015-11-02 10:09:41 -0500

    add bower routes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
-    fs = require('fs');
+    fs = require('fs'),
+    imageMagick = require('imagemagick-native');
 
 var app = express();
 
@@ -16,11 +17,23 @@ app.use('/bower_components', express.static('./bower_components'));
 
 //simple api to return image links
 app.get('/api/backgrounds', function(req,res){
-	fs.readdir('./public/img', function(err, data){
+	fs.readdir('./public/optimized', function(err, data){
+		data = data.map(function(file){return{url:'optimized/' + file}});
 		res.json({data:data});
 	})
 	
+});
+
+app.get('/api/icons', function(req,res){
+	fs.readdir('./public/icons', function(err, data){
+		data = data.map(function(file){return {url:'icons/' + file, description:file.replace('.svg', '')}});
+		// console.log(data);
+		res.json({data:data});
+	})
 })
+
+//test imagemagic
+
 
 //listen
 app.listen(8080);
