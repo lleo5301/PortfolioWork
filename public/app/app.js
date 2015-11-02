364ccd072f312b5e463f7f059f06a50fed46db80
leonardo Quintero <lleo5301@gmail.com>
2015-11-02 10:10:57 -0500

    add governmentstats project route

--- public/app/app.js
+++ public/app/app.js
@@ -2,25 +2,29 @@ var app = angular.module('portfolio', ['ngRoute', 'ngAnimate']);
 
 //config
 app.config(function($routeProvider, $locationProvider){
-
+  
 	//routes
 	$routeProvider.when('/About',{
 		templateUrl:'/views/about.html',
 		controller: 'aboutController'
 	}).
-	when('/Work',{
+	when('/work',{
 		templateUrl:'/views/work.html',
 		controller: 'workController'
 	}).
-	when('/Blog', {
+	when('/blog', {
 		templateUrl:'/views/blog.html',
 		controller: 'blogController'
-	}).otherwise({
+	}).
+    when('/projects/governmentStats',{
+        templateUrl:'views/governmentstats.html',
+        controller: 'statsController'
+    }).
+    otherwise({
 		templateUrl:'views/home.html',
 		controller:'homeController'
 	})
 
-
-
-	$locationProvider.html5Mode(true);
 })
+
+
